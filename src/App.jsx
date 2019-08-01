be5b41f589bc9f4353821fab92a0e76e597e1a3c
DzinnApp <dzinnapp@gmail.com>
2019-08-01 09:06:14 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,189 @@
+/* eslint-disable no-param-reassign,jsx-a11y/label-has-for */
+import React, { Component } from 'react';
+import Loadable from 'react-loadable';
+
+import Loading from './Loading';
+import './assets/scss/styles.scss';
+import HeaderBar from './components/HeaderBar';
+import Footer from './sections/FooterSection';
+
+
+const AboutSection = Loadable({
+  loader: () => import(/* webpackChunkName: "AboutSection" */ './sections/AboutSection'),
+  loading: Loading,
+});
+
+const ProjectSection = Loadable({
+  loader: () => import(/* webpackChunkName: "ProjectSection" */ './sections/ProjectSection'),
+  loading: Loading,
+});
+
+const SkillCardSection = Loadable({
+  loader: () => import(/* webpackChunkName: "SkillCardSection" */ './sections/SkillCardSection'),
+  loading: Loading,
+});
+
+const EducationSection = Loadable({
+  loader: () => import(/* webpackChunkName: "EducationSection" */ './sections/EducationSection'),
+  loading: Loading,
+});
+
+const ExperienceSection = Loadable({
+  loader: () => import(/* webpackChunkName: "ExperienceSection" */ './sections/ExperienceSection'),
+  loading: Loading,
+});
+
+// const ParticleBg = Loadable({
+//   loader: () => import(/* webpackChunkName: "ParticleBg" */ './ParticleBg'),
+//   loading: Loading,
+// });
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      introText: `
+        I'm a Bengaluru (India) based developer.
+        I work on React.JS, React Native, Node.JS, Python, Android, PHP, Angular.
+      `,
+
+      lastUpdated: null,
+    };
+    window.addEventListener('load', this.callbackFunc);
+    window.addEventListener('scroll', this.callbackFunc);
+  }
+
+
+  componentDidMount() {
+    fetch('https://api.github.com/repos/kaushiknishchay/kaushiknishchay.github.io').then(
+      resp => resp.json(),
+    ).then((response) => {
+      const lastUpdated = response.updated_at;
+      this.setState({
+        lastUpdated: new Date(lastUpdated).toDateString(),
+      });
+    }).catch(() => {
+      // Error
+    });
+  }
+
+
+  callbackFunc = () => {
+    const items = document.querySelectorAll(
+      '.timeline--container > ul.timeline--line > li > .timeline_item--content',
+    );
+      console.log(items);
+      
+    for (let i = 0; i < items.length; i += 1) {
+      if (
+        this.elementInViewport2(items[i])
+        || this.isElementInViewport(items[i])
+      ) {
+        items[i].parentElement.classList.add('in-view');
+      }
+    }
+  };
+
+  elementInViewport2 = (el) => {
+    let top = el.offsetTop;
+    let left = el.offsetLeft;
+    const width = el.offsetWidth;
+    const height = el.offsetHeight;
+
+    while (el.offsetParent) {
+      el = el.offsetParent;
+      top += el.offsetTop;
+      left += el.offsetLeft;
+    }
+
+    return (
+      top < window.pageYOffset + window.innerHeight
+      && left < window.pageXOffset + window.innerWidth
+      && top + height > window.pageYOffset
+      && left + width > window.pageXOffset
+    );
+  };
+
+  isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0
+      && rect.left >= 0
+      && rect.bottom
+      <= (window.innerHeight || document.documentElement.clientHeight)
+      && rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  render() {
+    const {
+      introText,
+      lastUpdated,
+    } = this.state;
+
+    return (
+      <div className="content">
+
+        <div className="header-wrap" role="main" aria-label="Nishchay Kaushik introduction.">
+          <HeaderBar />
+          <div className="heading--title">
+            <h1 className="main--heading">
+
+
+              Nishchay Kaushik
+            </h1>
+            <h2 className="sub--heading">
+
+
+              Software Development Engineer - I
+            </h2>
+
+            <div className="description">
+              {introText}
+              <p
+                style={{
+                  marginTop: '3em',
+                }}
+              >
+                <a
+                  href="./resume_new.pdf"
+                  className="download_cv"
+                  onClick="ga('send', 'event', 'Download', 'resume', 'version 1');"
+                >
+
+
+                  Download Resume
+                </a>
+              </p>
+            </div>
+          </div>
+          {
+            lastUpdated
+            && (
+              <span className="last--updated">
+
+
+                Last Updated at:
+                {' '}
+                {lastUpdated || ''}
+              </span>
+            )
+          }
+        </div>
+
+        <ExperienceSection />
+
+        <SkillCardSection />
+
+        <ProjectSection />
+
+        <EducationSection />
+
+        <AboutSection />
+        <Footer />
+      </div>
+    );
+  }
+}
---
+++ src/constants/experiences.js
@@ -0,0 +1,67 @@
+export default [
+  {
+    title: 'Software Development Engineer - I',
+    subTitle: 'HackerRank',
+    date: '2019 - PRESENT',
+    desc: 'Working on React.JS, Backbone.JS, CoffeeScript and a lot more.',
+  },
+  {
+    title: 'Software Developer',
+    subTitle: 'CodeBrahma Technologies',
+    date: 'MARCH 2018 - JANUARY 2019',
+    desc: 'Worked as a React.JS developer to create WebApps and PWAs and as a React Native developer to create mobile apps both for Android and iOS.',
+  },
+  {
+    title: 'Assistant System Engineer',
+    subTitle: 'Tata Consultancy Services',
+    date: 'FEB 2017 - MARCH 2018',
+    desc:
+      'Oracle Siebel Developer. Unit Tested and worked on Siebel OpenUI Framework.',
+  },
+  {
+    title: 'Freelance Developer',
+    subTitle: '',
+    date: '2011 - JAN 2017',
+    desc: `
+          FullStack Developer and Android App Developer (Native & Hybrid)
+          <br/><br/>
+          <b>Responsibilities :</b>
+          <br/>
+          <ul class="desc__list">
+          <li>
+            Development of Web Applications using latest frontend / backend technologies.
+          </li>
+          <li>
+            Have knowledge of designing and integrating Angular 2 Modules or use bootstrap.
+          </li>
+          <li>
+            Have knowledge in PHP and in using Laravel framework for backend development.
+          </li>
+          <li>
+            Able to design and create Android Apps be it Native or Hybrid (Ionic) and publish them on Play-Store.
+          </li>
+          <li>
+            Designing REST APIs.
+          </li>
+          <li>
+            Implementing token based authentication  (JWT) in Node.JS.
+          </li>
+          </ul>
+
+          <br/><br/>
+          <b>Key Working Areas :</b>
+          <ul class="desc__list">
+          <li>
+          Frontend : Angular 2/4, Angular Material, Bootstrap, HTML5, CSS3, JavaScript, TypeScript, React.JS, jQuery
+          </li>
+          <li>Backend : PHP, Node.JS</li>
+          <li>App Development : Android, React Native</li>
+          <li>Frameworks : Laravel (PHP), Express (NodeJS)</li>
+          <li>Database : MySQL, MariaDB, Mongoose, MongoDB</li>
+          <li>Software Version Control : Git</li>
+          <li>Tools : Notepad++, Eclipse, Android Studio, PHPStorm, PyCharms, WebStorm</li>
+          <li>Languages : Python, Java</li>
+          </ul>
+        `,
+  },
+];
